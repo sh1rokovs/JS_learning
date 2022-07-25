2d5876282a19f2611c335305209ff44ea04dc0bc
sh1rokovs <hilallod@yandex.ru>
2022-07-25 18:27:42 +0300

    tsk3

--- task_3/script.js
+++ task_3/script.js
@@ -1,5 +1,26 @@
-var readlineSync = require('readline-sync');
+const readlineSync = require('readline-sync');
  
-// Wait for user's response.
-var userName = readlineSync.question('May I have your name? ');
-console.log('Hi ' + userName + '!');
+// let countOfValue = readlineSync.question('Введите размер числа: ');
+// let countOfTry = readlineSync.question('Введите количество попыток(Неограниченно = 0):')
+
+function checkWin(valueRobot, valueHuman, countOfValue) {
+    let countOfCow = 0 // Цифры совпадают и на своих местах
+    let countOfBulls = 0 // Такая цифра есть в числе, но не на своем месте
+    for(let i = 0; i < countOfValue; i++) {
+        if(valueRobot.toString()[i] == valueHuman.toString()[i]) {
+            countOfCow++
+        }
+        else {
+            let valueRobot1 = valueRobot.toString()
+            valueRobot1[i] = "*"
+            if(valueRobot1.includes(valueHuman.toString()[i]))
+            {
+                countOfBulls++
+            }
+        }
+    }
+    console.log(countOfCow)
+    console.log(countOfBulls)
+}
+
+checkWin(44443, 33334, 5)
