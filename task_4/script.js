df0b1d8b7540fd255ca00cb36a8f2abd5bf25190
sh1rokovs <hilallod@yandex.ru>
2022-07-29 15:27:31 +0300

    tsk4

--- task_4/script.js
+++ task_4/script.js
@@ -1,24 +1,74 @@
-const products = [
+class ProductList {
+    constructor(container = ".products"){
+        this.container = container
+        this.goods = []
+        this.allProducts = []
+        this.#fetchGoods()
+        this.render()
+    }
+
+    #fetchGoods() {
+        this.goods = [
+            {id: 1, title: 'Notebook', price: 20000},
+            {id: 2, title: 'Mouse', price: 1500},
+            {id: 3, title: 'Keybord', price: 5000},
+            {id: 4, title: 'Gamepad', price: 4500},
+        ]
+    }
+
+    render() {
+        const block = document.querySelector(this.container)
+
+        for (let product of this.goods) {
+            const productObject = new ProductItem(product)
+
+            this.allProducts.push(productObject)
+            block.insertAdjacentHTML('beforeend', productObject.render())
+        }
+    }
+}
+
+class ProductItem {
+    constructor(product, img="") {
+        this.title = product.title
+        this.price = product.price
+        this.id = product.id
+        this.img = img
+    }
+
+    render() {
+        return `<div class="product-item col" data-id="${this.id}">
+        <center>
+        <img src="${this.img}" alt="some img">
+        <h3 class="">${this.title}</h3>
+        <p class="">${this.price} \u20bd</p>
+        <button class="buy-btn">Купить</button>
+        </div>
+        </center>`
+    }
+}
+
+new ProductList()
+
+/* const products = [
     {id: 1, title: 'Notebook', price: 20000},
     {id: 2, title: 'Mouse', price: 1500},
     {id: 3, title: 'Keybord', price: 5000},
     {id: 4, title: 'Gamepad', price: 4500},
 ]
 
-const renderProduct = (title="Нет титула", price="Нет цены") => {
-    return `<div class="product-item col">
+const renderProduct = (item, img="") => `<div class="product-item col" data-id="${this.id}">
                 <center>
-                <h3 class="">${title}</h3>
-                <p class="">${price}</p>
+                <img src="${img}" alt="some img">
+                <h3 class="">${item.title}</h3>
+                <p class="">${item.price}</p>
                 <button class="by-btn">Добавить в корзину</button>
                 </div>
-                </center>
-    `
-}
+                </center>`
 
 const renderProducts = (list) => {
-    const productList = list.map(product => renderProduct(product.title, product.price))
-    document.querySelector(".products").innerHTML = productList.join(" ")
+    const productList = list.map(item => renderProduct(item))
+    document.querySelector(".products").insertAdjacentHTML('beforeend', list.map(item => renderProduct(item)).join(" "))
 }
 
-renderProducts(products)
+renderProducts(products) */
